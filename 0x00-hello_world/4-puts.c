71e054aa5a12cfa82449312e28971905dfdd320b
GhadeerAbdEl-Monem <ghadeermansour66@gmail.com>
2024-06-20 15:08:12 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- *  new program
+ * main - Entry point
  *
- *   
- *   
- */   
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
